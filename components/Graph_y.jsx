72b773cf73533d275c8f0941f5025b604e0bf7e5
unknown <tanishm.it.20@nitj.ac.in>
2024-04-02 02:20:57 +0530

    all the variables graph plot done

--- components/Graph_y.jsx
+++ components/Graph_y.jsx
@@ -1,7 +1,7 @@
 
 import { useContext } from 'react';
 import { GraphContext } from '../context/context';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -17,6 +17,7 @@ import { Line } from 'react-chartjs-2';
 import moment from 'moment';
 
 import { SimpleLinearRegression } from 'ml-regression-simple-linear';
+import { Button } from 'react-bootstrap';
 
 
 
@@ -57,7 +58,7 @@ const calculateFg = (a, h) => 8*a + 3*h;
 const Graph_y = ({dataset, datasetbar}) => {
 
   
-
+  const [showVariables, setShowVariables] = useState(false); 
   
 
   const { modifiedFgData, modifiedDatasetA, updateModifiedFg } = useContext(GraphContext);
@@ -73,6 +74,26 @@ const Graph_y = ({dataset, datasetbar}) => {
   // Calculate fy values
   const dataset1 = [];
 
+  const formulas = {
+    fy: {
+      formula: "d + 2 * e + g",
+      dependencies: ["d", "e", "a", "h"],
+    },
+
+    // ... add more formulas here
+  };
+
+  const allVariables = new Set(); 
+  const extractVariables = (formulaName) => {
+    const formula = formulas[formulaName];
+    formula.dependencies.forEach((dep) => {
+      allVariables.add(dep);
+      if (dep in formulas) { // Check if the dependency is another formula
+        extractVariables(dep);  // Recursively extract
+      }
+    });
+  };
+  extractVariables("fy"); 
 
   
   dataset.forEach(item => {
@@ -121,39 +142,70 @@ const deviations = dataset1.map((fyValue, index) => {
 
 
 
+  const colors = [
+    "rgb(255, 99, 132)",
+    "rgb(143, 0, 255)",
+    "rgb(0, 128, 255)",
+    "rgb(255, 159, 64)",
+    "rgb(75, 192, 192)",
+  ];
+
+
+
   const data = {
     labels,
     datasets: [
-      {
-        label: 'Original Data',
-        data: dataset1,
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        tension: 0.5
-      },
-      ...(modifiedFgData ? [ 
         {
-            label: 'Modified F_y',
-            // Use values from modifiedFgData
-            data: modifiedFgData.map(item => calculateFy(item.d, item.e, item.value)),
-            // ... other styles
-            borderColor: 'rgb(143, 0, 255)',
-            backgroundColor: 'rgba(143, 0, 255, 0.5)',
+            label: 'Original Data',
+            data: dataset1,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
             tension: 0.5
-        }
-    ] : [])
-  
+        },
+        ...(modifiedFgData ? [ 
+            {
+                label: 'Modified F_y',
+                // Use values from modifiedFgData
+                data: modifiedFgData.map(item => calculateFy(item.d, item.e, item.value)),
+                // ... other styles
+                borderColor: 'rgb(143, 0, 255)',
+                backgroundColor: 'rgba(143, 0, 255, 0.5)',
+                tension: 0.5
+            }
+        ] : []),
+
+        ...(showVariables 
+            ? Array.from(allVariables).map((variable, index) => ({
+                  label: variable,
+                  data: dataset.map((item) => {
+                      if (variable in item) {
+                          return item[variable];
+                      } else {
+                          const foundBarData = datasetbar.find(
+                              (barItem) => barItem.date === item.date
+                          );
+                          return foundBarData ? foundBarData[variable] : null; 
+                      }
+                  }),
+                  borderColor: colors[index % colors.length],
+                  backgroundColor: `${colors[index % colors.length]}4D`,
+                  tension: 0.5 
+              })) 
+            : [] 
+        ), // <-- Added a comma here
     ],
-  };
+};
 
 
   return (
     
     <div> 
       <div>
+      <Button onClick={() => setShowVariables(true)}>find Spike</Button>
        
         <Line options={options} data={data} />
       </div>
+
     </div>
   );
 };
